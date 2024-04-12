4f94a57b57a19a58ea51f0eb8a64dcc64b6ae738
isidoridev <faircloughconor@gmail.com>
2024-04-12 19:46:15 +0100

    Fixed UserNameText not displaying

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -31,15 +31,23 @@ const DetailsTextDiv = styled.div`
   margin: 10px;
 `
 
-const Name = ({ val }: any): ReactNode => {
-  return <p><strong className="color pink">{val}</strong></p>;
+const UserNameText = ({ username }: any): ReactNode => {
+  return <p><strong>{username}</strong></p>;
 }
 
-/*
-const Interests = ({s}: any) => {
-  return s
+
+const InterestsDiv = styled.div`
+  display: inline;
+`
+
+const InterestText = styled.p`
+  display: inline;
+  margin-right: 40px;
+  color: red;
+`
+const BioText = ({ bio }: any): ReactNode => {
+  return <p><i style={{color: "pink"}}>{bio}</i></p>;
 }
-*/
 
 export function UserCard(): ReactNode {
   const user: UserInfo = {
@@ -54,16 +62,17 @@ export function UserCard(): ReactNode {
       <Card className="p-6 flex items-center">
         <UserImage src={user.image} />
         <DetailsTextDiv>
-          <Name name={user.name}/>
-          <div style={{display: "inline"}}>
+          <UserNameText username={user.name}/>
+          <InterestsDiv>
             <Link to={'/interests'}>
             {user.interests.map((i: String) => { 
               return (
-              <p style={{display: "inline", marginRight: "40px", color: "red"}}>
-                {i}
-              </p>)})}
+                <InterestText key={i}>
+                  {i}
+                </InterestText>
+              )})}
             </Link>
-          </div>
+          </InterestsDiv>
           <p><i style={{marginTop: "20px"}}>
             {user.bio}</i></p>
         </DetailsTextDiv>
